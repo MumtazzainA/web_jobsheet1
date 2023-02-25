9ac49767d7138ae742331993ba8130a7ad3bb9c8
MumtazzainA <zainabdullah2126@gmail.com>
2023-02-25 10:32:06 +0700

    Co-authored-by: MumtazzainA <zainabdullah2126@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <body class="antialiased">
             <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Mumtaz Zain A</a></div>
+                <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Mumtaz Zain</a></div>
             </div>
         </body>
 </html>
